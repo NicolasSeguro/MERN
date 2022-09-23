85282438ac61b6f0550b16fa5dc60a018a972782
NicolasSeguro <nicolas.seguro@gmail.com>
2022-09-23 14:52:03 -0300

    Manual component

---
+++ fundamentos/src/ManualComponent.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+
+const ManualComponent = () => {
+    {/* Agrego fragment para sumar etiquetas */}
+    return (
+        <>
+            <h1>Componente manual</h1>
+            <p>Probando el componente </p>
+        </>
+    );
+}
+
+export default ManualComponent
