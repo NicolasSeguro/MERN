f3181d7e7ea2cd0d910d486d0ba1dc294b134ba3
NicolasSeguro <nicolas.seguro@gmail.com>
2022-09-23 15:29:34 -0300

    Mostrando datos en la vista con variables y objeto

--- fundamentos/src/ManualComponent.js
+++ fundamentos/src/ManualComponent.js
@@ -2,11 +2,33 @@ import React from "react";
 
 
 const ManualComponent = () => {
+
+    {/* Muestro datos del componente en la vista o template */}
+    let nombre = 'Niko';
+    let web = 'nikoseguro.com'
+
+    let usuario = {
+        contenido: 'Portfolio personal',
+        contacto: 'hello@nikoseguro.com'
+
+    }
+
     {/* Agrego fragment para sumar etiquetas */}
     return (
         <>
             <h1>Componente manual</h1>
-            <p>Probando el componente </p>
+            <p>Datos desde variables:</p>
+            <ul>
+                {/* Con varibles */}
+                <li>Nombre: {nombre}</li>
+                <li>Sitio web: {web}</li>
+            </ul>
+            <p>Dato desde objeto: </p>
+            <ul>
+                <li>Contenido del sitio: {usuario.contenido} </li>
+                <li>Contacto: {usuario.contacto}</li>
+            </ul>
+
         </>
     );
 }
